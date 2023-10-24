35178779db3277898933af8673a07e40fcea06ba
osparking <jbpark03@gmail.com>
2023-10-25 03:53:21 +0900

    두 종속성을 더한 최초판

---
+++ src/main/java/sqch11ex2/SqCh11Ex2Application.java
@@ -0,0 +1,13 @@
+package sqch11ex2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh11Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh11Ex2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sqch11ex2/SqCh11Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sqch11ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh11Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

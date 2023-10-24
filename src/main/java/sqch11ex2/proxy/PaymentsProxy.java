df8e568e3fc199298d9934f12676f4f354d0ba05
osparking <jbpark03@gmail.com>
2023-10-25 04:24:02 +0900

    REST템플리트 사용하는 지불 대리자

---
+++ src/main/java/sqch11ex2/proxy/PaymentsProxy.java
@@ -0,0 +1,36 @@
+package sqch11ex2.proxy;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import sqch11ex2.dto.Payment;
+
+@Component
+@AllArgsConstructor
+public class PaymentsProxy {
+	private final RestTemplate template;
+	
+	public Payment createPayment(Payment payment) {
+		String uri = paymentServiceUrl + "/payment";
+		
+		var headers = new HttpHeaders();
+		headers.add("requestId", UUID.randomUUID().toString());
+		
+		var httpEntity = new HttpEntity<>(payment, headers);
+		var response = template.exchange(
+				uri, HttpMethod.POST, httpEntity, Payment.class);
+		
+		return response.getBody();
+	}
+	
+	@Value("${name.service.url}")
+	private String paymentServiceUrl;
+
+}
